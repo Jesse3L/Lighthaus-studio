641beee62d90307da4faa90ecef5bd183507f0df
Lighthaus Studio <natalie@lighthausstudio.com>
2026-04-17 21:00:15 -0500

    Fix: add compose suppressHydrationWarning to layout root to ignore browser extension mutations

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,9 +96,10 @@ export default function RootLayout({
   }
 
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <script
           type="application/ld+json"
